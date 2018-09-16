8e7d9aaa875476a7c5e29a296086538ab7b2beeb
OpticGenius <opticgenius@hotmail.com>
2018-09-16 20:11:34 +1000

    Added jumping to ball

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,18 @@ public class PlayerController : MonoBehaviour {
 
     public int radius;
 
+    public float jumpForce;
+
+    private bool onGround;
+
     // Use this for initialization
     private void Start () {
         rb = this.gameObject.GetComponent<Rigidbody> ();
 
         SphereCollider collider = this.gameObject.GetComponent<SphereCollider> ();
         collider.radius = radius;
+
+        onGround = true;
     }
 
     private void FixedUpdate () {
@@ -23,4 +29,19 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3 (horizontalMovement, 0.0f, forwardSpeed);
         rb.AddForce (movement);
     }
+
+    private void Update () {
+        Debug.Log (onGround);
+        if (Input.GetKeyDown ("space") && onGround) {
+            rb.AddForce (0.0f, jumpForce, 0.0f);
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter (Collision other) {
+        if (!onGround) {
+            onGround = true;
+        }
+    }
+
 }
