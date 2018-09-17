44d66f2d882e7259ad04c7b0987b245926136011
OpticGenius <opticgenius@hotmail.com>
2018-09-17 16:01:08 +1000

    Comments added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    // Player reference stored here
     public PlayerController playerController;
 
+    // Offset of camera behind player
     private float offset;
 
-    // Use this for initialization
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
     private void Start () {
         offset = transform.position.z - playerController.transform.position.z;
     }
-    // Update is called once per frame
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     private void Update () {
         float x = transform.position.x;
         float y = transform.position.y;
         float z = playerController.transform.position.z + offset;
 
+        // Update camera position
         transform.position = new Vector3 (x, y, z);
     }
 }
--- Assets/Scripts/SlopeGenerator.cs
+++ Assets/Scripts/SlopeGenerator.cs
@@ -5,36 +5,57 @@ using UnityEngine;
 
 public class SlopeGenerator : MonoBehaviour {
 
+    // Slope prefab passed in
     public GameObject prefab;
 
+    // The current slope being created
     private GameObject currentSlope;
 
+    // Length of slope
     public float slopeLength;
 
+    // Timer variables
     public float threshold;
-
     private float TimeCounter = 0.0f;
 
+    // All previous slopes inserted here
+    // TODO: Find a better way to handle this
     private List<GameObject> previousSlopes = new List<GameObject> ();
 
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
     private void Start () {
+        // Insert initial slope into previous slopes
         previousSlopes.Add (GameObject.FindGameObjectWithTag ("InitialSlope"));
 
+        // Instantiate a copy current slope with the prefab
         currentSlope = Instantiate (prefab) as GameObject;
         previousSlopes.Add (currentSlope);
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     private void Update () {
+
+        // If timer expires, generate a new slope
         if (Time.time - TimeCounter > threshold) {
             GenerateNewSlope ();
             TimeCounter = Time.time;
+
+            // Remove any slopes that we have passed
             RemoveSlopes ();
         }
     }
 
+    /// <summary>
+    /// Removes previous slopes that we've passed.
+    /// </summary>
     private void RemoveSlopes () {
         foreach (GameObject slope in previousSlopes.ToList ()) {
+
+            // If player distance greater than slope, destroy slope
             if (slope.transform.position.z < transform.position.z) {
                 previousSlopes.Remove (slope);
                 Destroy (slope);
@@ -42,16 +63,22 @@ public class SlopeGenerator : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Attaches new slope to infinite plane. 
+    /// </summary>
     private void GenerateNewSlope () {
+
+        // Update coordinates
         float x = currentSlope.transform.position.x;
         float y = currentSlope.transform.position.y;
         float z = currentSlope.transform.position.z + slopeLength;
 
+        // Update new position and rotation
         Vector3 newPosition = new Vector3 (x, y, z);
         Quaternion newRotation = currentSlope.transform.rotation;
 
+        // Instantiate a new prefab slope
         currentSlope = Instantiate (prefab, newPosition, newRotation) as GameObject;
-
         previousSlopes.Add (currentSlope);
     }
 }
