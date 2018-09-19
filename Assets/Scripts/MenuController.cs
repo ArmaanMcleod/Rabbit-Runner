048e6791eb0a25418302c1c02853a8d9529e8735
AL <alice.lin96@gmail.com>
2018-09-19 17:35:17 +1000

    set up very basic and ugly main menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuController : MonoBehaviour {
+	public string mainGameScene; 
+
+
+	//Changes from the main menu scene to the main game scene to start a new game
+	public void StartNewGame () {
+		SceneManager.LoadScene(mainGameScene);
+	}
+}
