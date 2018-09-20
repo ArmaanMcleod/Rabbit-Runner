cea0351cae8687a613f9596b7d9428047bbf101a
AL <alice.lin96@gmail.com>
2018-09-20 10:43:09 +1000

    added instructions scene

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,11 +5,22 @@ using UnityEngine.SceneManagement;
 
 
 public class MenuController : MonoBehaviour {
-	public string mainGameScene; 
+	public string mainGameScene;  //The string name of the main game scene
+	public string instructionsScene; //The string name of the instructions scene
+	public string mainMenuScene; //The string name of the main menu scene
 
 
 	//Changes from the main menu scene to the main game scene to start a new game
 	public void StartNewGame () {
 		SceneManager.LoadScene(mainGameScene);
+	}
+
+	//Changes from the main menu scene to the instructions scene
+	public void OpenInstructions(){
+		SceneManager.LoadScene(instructionsScene);
+	}
+
+	public void OpenMainMenu(){
+		SceneManager.LoadScene(mainMenuScene);
 	}
 }
