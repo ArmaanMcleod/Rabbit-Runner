cbf6ee808dbce0b35db6615548555321065514d6
AL <alice.lin96@gmail.com>
2018-09-21 13:50:00 +1000

    separated main menu and instruction controllers

---
+++ Assets/Scripts/SceneControllers/InstructionsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstructionsController : MonoBehaviour {
+
+	public string mainMenuScene; //The string name for the main menu scene
+
+	//Goes back to the main menu
+	public void GoBack(){
+		SceneManager.LoadScene(mainMenuScene);
+	}
+
+	
+}
--- Assets/Scripts/SceneControllers/MenuController.cs
+++ Assets/Scripts/SceneControllers/MenuController.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour {
 	public string mainGameScene;  //The string name of the main game scene
 	public string instructionsScene; //The string name of the instructions scene
-	public string mainMenuScene; //The string name of the main menu scene
 
 
 	//Switches to the main game scene to start a new game
@@ -18,10 +17,5 @@ public class MenuController : MonoBehaviour {
 	//Switches to the instructions scene
 	public void OpenInstructions(){
 		SceneManager.LoadScene(instructionsScene);
-	}
-
-	//Swtiches to the main menu scene
-	public void OpenMainMenu(){
-		SceneManager.LoadScene(mainMenuScene);
 	}
 }
