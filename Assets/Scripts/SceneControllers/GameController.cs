80248afd644d0004d9dd4909167f5cfacdea21ad
AL <alice.lin96@gmail.com>
2018-09-24 18:09:22 +1000

    pausing and unpausing scene works

---
+++ Assets/Scripts/SceneControllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public GameObject pauseCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.P)){
+			if(pauseCanvas.activeSelf){
+				UnPauseGame();
+			}else{
+				PauseGame();
+			}
+			
+		}
+	}
+
+	void PauseGame(){
+		Time.timeScale=0;
+		pauseCanvas.SetActive(true);
+
+	}
+
+	public void UnPauseGame(){
+		Time.timeScale=1;
+		pauseCanvas.SetActive(false);
+	}
+}
