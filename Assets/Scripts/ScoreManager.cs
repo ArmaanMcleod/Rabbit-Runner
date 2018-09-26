6815643a666b2fb6915b56a0813721006e213d98
AL <alice.lin96@gmail.com>
2018-09-26 12:14:09 +1000

    added how to play instructions

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,19 +10,29 @@ public class ScoreManager : MonoBehaviour {
 	// Text object which displays the score in the game
 	public Text scoreText;
 
+	// Player's score data
 	private ScoreData scoreData;
+
+	// Game controller
+	private MainGameController gameController;
 
 	/// <summary>
 	/// Initialisation
 	/// </summary>
 	void Start () {
 		scoreData = gameObject.GetComponent<ScoreData>();
+		gameController = gameObject.GetComponent<MainGameController>();
 	}
 
 	/// <summary>
 	/// Update is called once per frame to update the player's score
 	/// </summary>
 	void Update () {
+		// Don't update if game is over
+		if(gameController.isGameOver()||gameController.isPaused()){
+			return;
+		}
+
 		scoreData.updateScore();
 		scoreText.text = scoreData.getScore().ToString();
 	}
