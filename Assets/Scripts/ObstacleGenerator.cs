47e778bffe67ed0cccd766d69fbc3af9573ee72b
OpticGenius <opticgenius@hotmail.com>
2018-09-29 12:55:34 +1000

    Added standard assets

--- Assets/Scripts/ObstacleGenerator.cs
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class ObstacleGenerator : MonoBehaviour {
 
     private ObstacleSection obstacleSection;
-    
-    // Use this for initialization
-    void Start() {
-        Vector3 position = gameObject.transform.position;
-        Vector3 size = gameObject.GetComponent<Renderer>().bounds.size;
 
-        this.obstacleSection = new ObstacleSection(position, size.x, size.z);
+    // Use this for initialization
+    void Start () {
+        Vector3 position = gameObject.transform.position;
+        Vector3 size = gameObject.GetComponent<Renderer> ().bounds.size;
+
+        this.obstacleSection = new ObstacleSection (position, size.x, size.z);
     }
 
     /// <summary>
     /// Updates the obstacles to lie at the new location.
     /// </summary>
-    public void UpdateObstacles() {
+    public void UpdateObstacles () {
         Vector3 position = gameObject.transform.position;
-        obstacleSection.UpdateCoordinates(position);
+        obstacleSection.UpdateCoordinates (position);
     }
 }
