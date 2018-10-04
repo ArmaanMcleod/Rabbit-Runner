c6d5b225c0721ae36a190920b462236dacea53ae
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-10-04 17:52:02 +1000

    Better distribution of obstacles

--- Assets/Scripts/ChunkGenerator.cs
+++ Assets/Scripts/ChunkGenerator.cs
@@ -35,71 +35,74 @@ public class ChunkGenerator : MonoBehaviour {
     /// <summary>
     /// Awake is used to initialize any variables or game state before the game starts.
     /// </summary>
-    private void Awake () {
+    private void Awake() {
 
         // Create chunk queue 
-        InitializeChunks ();
+        InitializeChunks();
 
         // Set the game controller
-        gameController = gameControllerObj.GetComponent<MainGameController> ();
+        gameController = gameControllerObj.GetComponent<MainGameController>();
     }
 
     /// <summary>
     /// Update is called once per frame
     /// </summary>
-    private void Update () {
+    private void Update() {
         // Do not update if game is paused or over
-        if (gameController.isGameOver () || gameController.isPaused ()) {
+        if (gameController.isGameOver() || gameController.isPaused()) {
             return;
         }
 
         // Update current chunk and index
-        currentChunk = GetCurrentChunk ();
-        currentChunkIndex = GetCurrentChunkIndex ();
+        currentChunk = GetCurrentChunk();
+        currentChunkIndex = GetCurrentChunkIndex();
 
         // Activate new chunks
-        ActivateChunks ();
+        ActivateChunks();
 
         // Delete previous chunks
-        SweepPreviousChunks ();
+        SweepPreviousChunks();
     }
 
     /// <summary>
     /// Initialise chunk queue.
     /// </summary>
-    private void InitializeChunks () {
-        chunks = new Queue<GameObject> ();
+    private void InitializeChunks() {
+        chunks = new Queue<GameObject>();
 
         for (int i = 0; i < numberOfChunks; i++) {
-            GameObject chunk = Instantiate (chunkPrefab) as GameObject;
-            chunk.transform.position = CalculateNextChunkPosition ();
+            GameObject chunk = Instantiate(chunkPrefab) as GameObject;
+            chunk.transform.position = CalculateNextChunkPosition();
+            if (chunk.tag == "Slope" && i != 0) {
+                chunk.GetComponent<ObstacleGenerator>().UpdateObstacles();
+            }
 
             // All chunks except first are activated
             if (i != 0) {
-                chunk.SetActive (false);
+                chunk.SetActive(false);
             }
 
-            chunks.Enqueue (chunk);
+            chunks.Enqueue(chunk);
         }
     }
 
     /// <summary>
     /// Activate chunks left in hierachy
     /// </summary>
-    private void ActivateChunks () {
+    private void ActivateChunks() {
         int threshold = currentChunkIndex + activeChunks;
 
         for (int i = currentChunkIndex; i < threshold; i++) {
-            int minimumIndex = Mathf.Clamp (i, 0, chunks.Count - 1);
-            GameObject chunk = chunks.ElementAt (minimumIndex);
+            int minimumIndex = Mathf.Clamp(i, 0, chunks.Count - 1);
+            GameObject chunk = chunks.ElementAt(minimumIndex);
 
             // If not activiated in hierachy, activate it
             if (!chunk.activeInHierarchy) {
-                chunk.SetActive (true);
+                chunk.SetActive(true);
 
                 // Only add obstacles on slopes
                 if (chunk.tag == "Slope") {
-                    chunk.GetComponent<ObstacleGenerator> ().UpdateObstacles ();
+                    chunk.GetComponent<ObstacleGenerator>().UpdateObstacles();
                 }
             }
         }
@@ -109,7 +112,7 @@ public class ChunkGenerator : MonoBehaviour {
     /// Calculates next chunk position.
     /// </summary>
     /// <returns>New chunk position in world</returns>
-    private Vector3 CalculateNextChunkPosition () {
+    private Vector3 CalculateNextChunkPosition() {
         Vector3 newPosition = chunkPrefab.transform.position;
         newPosition.z = currentChunkPosition;
         currentChunkPosition += chunkLength;
@@ -119,15 +122,15 @@ public class ChunkGenerator : MonoBehaviour {
     /// <summary>
     /// Sweeps aside chunks we have passed.
     /// </summary>
-    private void SweepPreviousChunks () {
+    private void SweepPreviousChunks() {
         // Sweep aside previous chunks were finished with
         if (currentChunkIndex > 0) {
-            Vector3 previousPosition = chunks.ElementAt (currentChunkIndex - 1).transform.position;
-            float distance = Vector3.Distance (transform.position, previousPosition);
+            Vector3 previousPosition = chunks.ElementAt(currentChunkIndex - 1).transform.position;
+            float distance = Vector3.Distance(transform.position, previousPosition);
 
             // If distance is greater, recycle chunk
             if (distance > (chunkLength * distanceRatio)) {
-                RecycleChunk ();
+                RecycleChunk();
             }
         }
     }
@@ -135,25 +138,25 @@ public class ChunkGenerator : MonoBehaviour {
     /// <summary>
     /// Recycle chunk to be used again.
     /// </summary>
-    private void RecycleChunk () {
-        GameObject chunk = chunks.Dequeue ();
-        chunk.transform.position = CalculateNextChunkPosition ();
+    private void RecycleChunk() {
+        GameObject chunk = chunks.Dequeue();
+        chunk.transform.position = CalculateNextChunkPosition();
 
-        chunk.SetActive (false);
-        chunks.Enqueue (chunk);
+        chunk.SetActive(false);
+        chunks.Enqueue(chunk);
     }
 
     /// <summary>
     /// Gets current chunk game object player is on in world.
     /// </summary>
     /// <returns>The chunk player is on.</returns>
-    private GameObject GetCurrentChunk () {
+    private GameObject GetCurrentChunk() {
         GameObject newCurrentChunk = null;
 
         foreach (GameObject chunk in chunks) {
 
             // If we are at the midpoint of chunk, were on this chunk
-            if (Vector3.Distance (transform.position, chunk.transform.position) <= (chunkLength / 2)) {
+            if (Vector3.Distance(transform.position, chunk.transform.position) <= (chunkLength / 2)) {
                 newCurrentChunk = chunk;
                 break;
             }
@@ -166,14 +169,14 @@ public class ChunkGenerator : MonoBehaviour {
     /// Gets index of current chunk in world
     /// </summary>
     /// <returns>The index of the current chunk</returns>
-    private int GetCurrentChunkIndex () {
+    private int GetCurrentChunkIndex() {
         int index = 0;
 
         for (int i = 0; i < chunks.Count; i++) {
-            GameObject chunk = chunks.ElementAt (i);
+            GameObject chunk = chunks.ElementAt(i);
 
             // If object is the same, this is the current chunk
-            if (chunk.Equals (currentChunk)) {
+            if (chunk.Equals(currentChunk)) {
                 index = i;
                 break;
             }
--- Assets/Scripts/ObstacleSection.cs
+++ Assets/Scripts/ObstacleSection.cs
@@ -20,10 +20,10 @@ public class ObstacleSection {
     /// <summary>
     /// References to the cube and cylinder prefabs.
     /// </summary>
-    private readonly GameObject CONIFER_PREFAB = Resources.Load<GameObject> ("Prefabs/ObstaclePrefabs/Tree_Conifer_01");
-    private readonly GameObject ROCK_PREFAB = Resources.Load<GameObject> ("Prefabs/ObstaclePrefabs/Rock_Chunk_01");
-    private readonly GameObject HARE_PREFAB = Resources.Load<GameObject> ("Prefabs/ObstaclePrefabs/SCharacter_Turtle");
-    private readonly GameObject BIRD_PREFAB = Resources.Load<GameObject> ("Prefabs/ObstaclePrefabs/SCharacter_Bird1");
+    private readonly GameObject CONIFER_PREFAB = Resources.Load<GameObject>("Prefabs/ObstaclePrefabs/Tree_Conifer_01");
+    private readonly GameObject ROCK_PREFAB = Resources.Load<GameObject>("Prefabs/ObstaclePrefabs/Rock_Chunk_01");
+    private readonly GameObject TURTLE_PREFAB = Resources.Load<GameObject>("Prefabs/ObstaclePrefabs/SCharacter_Turtle");
+    private readonly GameObject BIRD_PREFAB = Resources.Load<GameObject>("Prefabs/ObstaclePrefabs/SCharacter_Bird1");
 
     /// <summary>
     /// Position of this section's slope.
@@ -44,62 +44,57 @@ public class ObstacleSection {
     /// These two fields hold references to the obstacles so that they can be
     /// recycled.
     /// </summary>
-    private List<GameObject> rocks = new List<GameObject> ();
-    private List<GameObject> conifers = new List<GameObject> ();
-    private List<GameObject> hares = new List<GameObject> ();
-    private List<GameObject> birds = new List<GameObject> ();
+    private List<GameObject> rocks = new List<GameObject>();
+    private List<GameObject> conifers = new List<GameObject>();
+    private List<GameObject> turtles = new List<GameObject>();
+    private List<GameObject> birds = new List<GameObject>();
 
-    private List<GameObject> currentObstacles = new List<GameObject> ();
+    private List<GameObject> currentObstacles = new List<GameObject>();
 
-    public ObstacleSection (Vector3 position, float xLen, float zLen) {
+    public ObstacleSection(Vector3 position, float xLen, float zLen) {
         this.position = position;
         this.xLen = xLen;
         this.zLen = zLen;
 
-        InstantiateObstacles (rocks, ROCK_PREFAB, NUM_ROCKS);
-        InstantiateObstacles (conifers, CONIFER_PREFAB, NUM_CONIFERS);
-        InstantiateObstacles (hares, HARE_PREFAB, NUM_HARES);
-        InstantiateObstacles (birds, BIRD_PREFAB, NUM_BIRDS);
-
-
-        RandomiseObstacles ();
+        InstantiateObstacles(rocks, ROCK_PREFAB, NUM_ROCKS);
+        InstantiateObstacles(conifers, CONIFER_PREFAB, NUM_CONIFERS);
+        InstantiateObstacles(turtles, TURTLE_PREFAB, NUM_HARES);
+        InstantiateObstacles(birds, BIRD_PREFAB, NUM_BIRDS);
     }
 
     /// <summary>
     /// Updates the coordinates of this section and randomises the obstacle locations.
     /// </summary>
     /// <param name="position">The new position for this section of obstacles.</param>
-    public void UpdateCoordinates (Vector3 position) {
+    public void UpdateCoordinates(Vector3 position) {
         this.position = position;
-        RandomiseObstacles ();
+        RandomiseObstacles();
     }
 
     /// <summary>
     /// Randomises the obstacles locations and make-up of 
     /// </summary>
-    private void RandomiseObstacles () {
-        
-
+    private void RandomiseObstacles() {
         // Clear the last set of obstacles
-        currentObstacles.ForEach (obj => obj.SetActive (false));
-        currentObstacles = new List<GameObject> ();
+        currentObstacles.ForEach(obj => obj.SetActive(false));
+        currentObstacles = new List<GameObject>();
 
         // Pick some random obstacles
         int rockIndex = 0;
         int coniferIndex = 0;
-        int hareIndex = 0;
+        int turtleIndex = 0;
         int birdIndex = 0;
-        
-        for(int i = 0; i < NUM_PER_SECTION; i++) {
-            float randomValue = UnityEngine.Random.value;
-            
-            if(randomValue < 0.05 && birdIndex<NUM_BIRDS){
+
+        for (int i = 0; i < NUM_PER_SECTION; i++) {
+            float randomValue = UnityEngine.Random.Range(0, 1);
+
+            if (randomValue < 0.05 && birdIndex < NUM_BIRDS) {
                 ActivateObstacle(birds[birdIndex], 10);
                 birdIndex++;
-            } else if (randomValue < 0.1 && hareIndex<NUM_HARES) {
-                ActivateObstacle(hares[hareIndex], 0);
-                hareIndex++;
-            } else if (randomValue < 0.3 && coniferIndex<NUM_CONIFERS) {
+            } else if (randomValue < 0.1 && turtleIndex < NUM_HARES) {
+                ActivateObstacle(turtles[turtleIndex], 0);
+                turtleIndex++;
+            } else if (randomValue < 0.3 && coniferIndex < NUM_CONIFERS) {
                 ActivateObstacle(conifers[coniferIndex], 0);
                 coniferIndex++;
             } else {
@@ -114,7 +109,7 @@ public class ObstacleSection {
     /// </summary>
     /// <param name="obstacle">The obstacle to be activated.</param>
     /// <param name="maxZPos">The obstacle's position on the y=axis</param>
-    private void ActivateObstacle(GameObject obj, float yPos){
+    private void ActivateObstacle(GameObject obj, float yPos) {
         currentObstacles.Add(obj);
         RandomisePosition(obj, yPos);
         obj.SetActive(true);
@@ -125,17 +120,17 @@ public class ObstacleSection {
     /// </summary>
     /// <param name="obstacle">The obstacle whose position will be randomised.</param>
     /// <param name="maxZPos">The obstacle's position on the y=axis</param>
-    private void RandomisePosition (GameObject obstacle, float yPos) {
+    private void RandomisePosition(GameObject obstacle, float yPos) {
         // The maximum and minimum possible X and Z positions of the obstacle
         float maxZPos = position.z + (zLen / 2);
         float minZPos = position.z - (zLen / 2);
-        float maxXPos = position.x + (xLen / 2);
-        float minXPos = position.x - (xLen / 2);
+        float maxXPos = position.x + (xLen / 2) - 1;
+        float minXPos = position.x - (xLen / 2) + 1;
 
-        float newX = UnityEngine.Random.Range (minXPos, maxXPos);
-        float newZ = UnityEngine.Random.Range (minZPos, maxZPos);
-        float newYRot = UnityEngine.Random.Range (0, 360);
-        obstacle.transform.position = new Vector3 (newX, yPos, newZ);
+        float newX = UnityEngine.Random.Range(minXPos, maxXPos);
+        float newZ = UnityEngine.Random.Range(minZPos, maxZPos);
+        float newYRot = UnityEngine.Random.Range(0, 360);
+        obstacle.transform.position = new Vector3(newX, yPos, newZ);
         obstacle.transform.rotation = Quaternion.Euler(new Vector3(0, newYRot, 0));
     }
 
@@ -145,11 +140,11 @@ public class ObstacleSection {
     /// <param name="obstacleList">The obstacles container to populate.</param>
     /// <param name="prefab">The obstacle prefab to instantiate from.</param>
     /// <param name="num">The number of prefabs to instantiate.</param>
-    private void InstantiateObstacles (List<GameObject> obstacleList, GameObject prefab, int num) {
+    private void InstantiateObstacles(List<GameObject> obstacleList, GameObject prefab, int num) {
         for (int i = 0; i < num; i++) {
-            GameObject gameObject = UnityEngine.Object.Instantiate (prefab);
-            gameObject.SetActive (false);
-            obstacleList.Add (gameObject);
+            GameObject gameObject = UnityEngine.Object.Instantiate(prefab);
+            gameObject.SetActive(false);
+            obstacleList.Add(gameObject);
         }
     }
 }
