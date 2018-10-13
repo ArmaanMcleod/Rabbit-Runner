c244299336246ddcb48c82f6ce13e5fc8634bff4
OpticGenius <opticgenius@hotmail.com>
2018-10-13 12:41:20 +1100

    Added turrets

--- Assets/Scripts/ObstacleSection.cs
+++ Assets/Scripts/ObstacleSection.cs
@@ -16,17 +16,19 @@ public class ObstacleSection {
     private readonly int NUM_CONIFERS = 2;
     private readonly int NUM_ROCKS = 10;
     private readonly int NUM_TURTLES = 2;
+    private readonly int NUM_TURRETS = 1;
 
     /// <summary>
     /// References to the cube and cylinder prefabs.
     /// </summary>
-    private readonly GameObject CONIFER_PREFAB = Resources.Load<GameObject>("Prefabs/ObstaclePrefabs/Tree_Conifer_01");
-    private readonly GameObject ROCK_PREFAB = Resources.Load<GameObject>("Prefabs/ObstaclePrefabs/Rock_Chunk_01");
-    private readonly GameObject TURTLE_PREFAB = Resources.Load<GameObject>("Prefabs/ObstaclePrefabs/SCharacter_Turtle");
-    private readonly GameObject BIRD_PREFAB = Resources.Load<GameObject>("Prefabs/ObstaclePrefabs/SCharacter_Bird1");
+    private readonly GameObject CONIFER_PREFAB = Resources.Load<GameObject> ("Prefabs/ObstaclePrefabs/Tree_Conifer_01");
+    private readonly GameObject ROCK_PREFAB = Resources.Load<GameObject> ("Prefabs/ObstaclePrefabs/Rock_Chunk_01");
+    private readonly GameObject TURTLE_PREFAB = Resources.Load<GameObject> ("Prefabs/ObstaclePrefabs/SCharacter_Turtle");
+    private readonly GameObject BIRD_PREFAB = Resources.Load<GameObject> ("Prefabs/ObstaclePrefabs/SCharacter_Bird1");
+    private readonly GameObject TURRET_PREFAB = Resources.Load<GameObject> ("Prefabs/SmallTurret");
 
-    private readonly GameObject HEALTH_ITEM_PREFAB = Resources.Load<GameObject>("Prefabs/HealthItem");
-    private readonly GameObject INVINCIBILITY_ITEM_PREFAB = Resources.Load<GameObject>("Prefabs/InvincibilityItem");
+    private readonly GameObject HEALTH_ITEM_PREFAB = Resources.Load<GameObject> ("Prefabs/HealthItem");
+    private readonly GameObject INVINCIBILITY_ITEM_PREFAB = Resources.Load<GameObject> ("Prefabs/InvincibilityItem");
 
     /// <summary>
     /// Position of this section's slope.
@@ -47,87 +49,94 @@ public class ObstacleSection {
     /// These two fields hold references to the obstacles so that they can be
     /// recycled.
     /// </summary>
-    private List<GameObject> rocks = new List<GameObject>();
-    private List<GameObject> conifers = new List<GameObject>();
-    private List<GameObject> turtles = new List<GameObject>();
-    private List<GameObject> birds = new List<GameObject>();
+    private List<GameObject> rocks = new List<GameObject> ();
+    private List<GameObject> conifers = new List<GameObject> ();
+    private List<GameObject> turtles = new List<GameObject> ();
+    private List<GameObject> birds = new List<GameObject> ();
+    private List<GameObject> turrets = new List<GameObject> ();
 
     private GameObject healthItem;
     private GameObject invincibilityItem;
 
-
-    private List<GameObject> currentObstacles = new List<GameObject>();
+    private List<GameObject> currentObstacles = new List<GameObject> ();
     private GameObject currentItem;
 
-    public ObstacleSection(Vector3 position, float xLen, float zLen) {
+    public ObstacleSection (Vector3 position, float xLen, float zLen) {
         this.position = position;
         this.xLen = xLen;
         this.zLen = zLen;
 
-        InstantiateObstacles(rocks, ROCK_PREFAB, NUM_ROCKS);
-        InstantiateObstacles(conifers, CONIFER_PREFAB, NUM_CONIFERS);
-        InstantiateObstacles(turtles, TURTLE_PREFAB, NUM_TURTLES);
-        InstantiateObstacles(birds, BIRD_PREFAB, NUM_BIRDS);
+        InstantiateObstacles (rocks, ROCK_PREFAB, NUM_ROCKS);
+        InstantiateObstacles (conifers, CONIFER_PREFAB, NUM_CONIFERS);
+        InstantiateObstacles (turtles, TURTLE_PREFAB, NUM_TURTLES);
+        InstantiateObstacles (birds, BIRD_PREFAB, NUM_BIRDS);
+        InstantiateObstacles (turrets, TURRET_PREFAB, NUM_TURRETS);
 
-        InstantiateItems();
+        InstantiateItems ();
     }
 
     /// <summary>
     /// Updates the coordinates of this section and randomises the obstacle locations.
     /// </summary>
     /// <param name="position">The new position for this section of obstacles.</param>
-    public void UpdateCoordinates(Vector3 position) {
-        Debug.Log("Updating coordinates");
+    public void UpdateCoordinates (Vector3 position) {
+        Debug.Log ("Updating coordinates");
         this.position = position;
-        RandomiseObstacles();
-        RandomiseItem();
+        RandomiseObstacles ();
+        RandomiseItem ();
     }
 
     /// <summary>
     /// Randomises the obstacles locations and make-up of 
     /// </summary>
-    private void RandomiseObstacles() {
+    private void RandomiseObstacles () {
         // Clear the last set of obstacles
-        currentObstacles.ForEach(obj => obj.SetActive(false));
-        currentObstacles = new List<GameObject>();
+        currentObstacles.ForEach (obj => obj.SetActive (false));
+        currentObstacles = new List<GameObject> ();
 
         // Pick some random obstacles
         int rockIndex = 0;
         int coniferIndex = 0;
         int turtleIndex = 0;
         int birdIndex = 0;
+        int turretIndex = 0;
 
         for (int i = 0; i < NUM_PER_SECTION; i++) {
-            float randomValue = UnityEngine.Random.Range(1, 100);
+            float randomValue = UnityEngine.Random.Range (1, 100);
+
+            if (randomValue < 5 && turretIndex < NUM_TURRETS) {
+                ActivateObstacle (turrets[turretIndex], 1);
+                turretIndex++;
+            }
 
             if (randomValue < 5 && birdIndex < NUM_BIRDS) {
-                ActivateObstacle(birds[birdIndex], 15);
+                ActivateObstacle (birds[birdIndex], 15);
                 birdIndex++;
             } else if (randomValue < 15 && turtleIndex < NUM_TURTLES) {
-                ActivateObstacle(turtles[turtleIndex], 0);
+                ActivateObstacle (turtles[turtleIndex], 0);
                 turtleIndex++;
             } else if (randomValue < 40 && coniferIndex < NUM_CONIFERS) {
-                ActivateObstacle(conifers[coniferIndex], 0);
+                ActivateObstacle (conifers[coniferIndex], 0);
                 coniferIndex++;
             } else {
-                ActivateObstacle(rocks[rockIndex], 0);
+                ActivateObstacle (rocks[rockIndex], 0);
                 rockIndex++;
             }
         }
     }
 
-    private void RandomiseItem() {
+    private void RandomiseItem () {
         if (currentItem != null) {
-            currentItem.SetActive(false);
+            currentItem.SetActive (false);
         }
 
-        UnityEngine.Random.InitState(DateTime.Now.Millisecond);
-        bool willSpawnItem = UnityEngine.Random.Range(1, 10) > 7.5;
+        UnityEngine.Random.InitState (DateTime.Now.Millisecond);
+        bool willSpawnItem = UnityEngine.Random.Range (1, 10) > 7.5;
         if (willSpawnItem) {
-            float val = UnityEngine.Random.Range(1, 10);
+            float val = UnityEngine.Random.Range (1, 10);
             currentItem = val < 5 ? invincibilityItem : healthItem;
-            RandomisePosition(currentItem, UnityEngine.Random.Range(0, 2.5f));
-            currentItem.SetActive(true);
+            RandomisePosition (currentItem, UnityEngine.Random.Range (0, 2.5f));
+            currentItem.SetActive (true);
         }
     }
 
@@ -136,10 +145,10 @@ public class ObstacleSection {
     /// </summary>
     /// <param name="obstacle">The obstacle to be activated.</param>
     /// <param name="maxZPos">The obstacle's position on the y=axis</param>
-    private void ActivateObstacle(GameObject obj, float yPos) {
-        currentObstacles.Add(obj);
-        RandomisePosition(obj, yPos);
-        obj.SetActive(true);
+    private void ActivateObstacle (GameObject obj, float yPos) {
+        currentObstacles.Add (obj);
+        RandomisePosition (obj, yPos);
+        obj.SetActive (true);
     }
 
     /// <summary>
@@ -147,26 +156,26 @@ public class ObstacleSection {
     /// </summary>
     /// <param name="obstacle">The obstacle whose position will be randomised.</param>
     /// <param name="maxZPos">The obstacle's position on the y=axis</param>
-    private void RandomisePosition(GameObject obstacle, float yPos) {
+    private void RandomisePosition (GameObject obstacle, float yPos) {
         // The maximum and minimum possible X and Z positions of the obstacle
         float maxZPos = position.z + (zLen / 2);
         float minZPos = position.z - (zLen / 2);
         float maxXPos = position.x + (xLen / 2) - 5;
         float minXPos = position.x - (xLen / 2) + 5;
 
-        float newX = UnityEngine.Random.Range(minXPos, maxXPos);
-        float newZ = UnityEngine.Random.Range(minZPos, maxZPos);
-        float newYRot = UnityEngine.Random.Range(0, 360);
-        obstacle.transform.position = new Vector3(newX, yPos, newZ);
-        obstacle.transform.rotation = Quaternion.Euler(new Vector3(0, newYRot, 0));
+        float newX = UnityEngine.Random.Range (minXPos, maxXPos);
+        float newZ = UnityEngine.Random.Range (minZPos, maxZPos);
+        float newYRot = UnityEngine.Random.Range (0, 360);
+        obstacle.transform.position = new Vector3 (newX, yPos, newZ);
+        obstacle.transform.rotation = Quaternion.Euler (new Vector3 (0, newYRot, 0));
     }
 
-    private void InstantiateItems() {
-        invincibilityItem = UnityEngine.Object.Instantiate(INVINCIBILITY_ITEM_PREFAB);
-        invincibilityItem.SetActive(false);
+    private void InstantiateItems () {
+        invincibilityItem = UnityEngine.Object.Instantiate (INVINCIBILITY_ITEM_PREFAB);
+        invincibilityItem.SetActive (false);
 
-        healthItem = UnityEngine.Object.Instantiate(HEALTH_ITEM_PREFAB);
-        healthItem.SetActive(false);
+        healthItem = UnityEngine.Object.Instantiate (HEALTH_ITEM_PREFAB);
+        healthItem.SetActive (false);
     }
 
     /// <summary>
@@ -175,11 +184,11 @@ public class ObstacleSection {
     /// <param name="obstacleList">The obstacles container to populate.</param>
     /// <param name="prefab">The obstacle prefab to instantiate from.</param>
     /// <param name="num">The number of prefabs to instantiate.</param>
-    private void InstantiateObstacles(List<GameObject> obstacleList, GameObject prefab, int num) {
+    private void InstantiateObstacles (List<GameObject> obstacleList, GameObject prefab, int num) {
         for (int i = 0; i < num; i++) {
-            GameObject gameObject = UnityEngine.Object.Instantiate(prefab);
-            gameObject.SetActive(false);
-            obstacleList.Add(gameObject);
+            GameObject gameObject = UnityEngine.Object.Instantiate (prefab);
+            gameObject.SetActive (false);
+            obstacleList.Add (gameObject);
         }
     }
 }
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour {
+    private GameObject player;
+    private LineRenderer lineRenderer;
+
+    public float lineHeightOffset;
+
+    private Color lineColour = Color.red;
+
+    private void Awake () {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        lineRenderer = gameObject.GetComponent<LineRenderer> ();
+        lineRenderer.enabled = false;
+        lineRenderer.material = new Material (Shader.Find ("Particles/Additive"));
+        lineRenderer.startColor = lineColour;
+        lineRenderer.endColor = lineColour;
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update () {
+        UpdateRotation ();
+        DrawLaser ();
+    }
+
+    /// <summary>
+    /// Draws laser to player
+    /// </summary>
+    private void DrawLaser () {
+        Vector3 newPosition = transform.position;
+        newPosition.y += lineHeightOffset;
+
+        lineRenderer.SetPosition (0, newPosition);
+        lineRenderer.SetPosition (1, player.transform.position);
+    }
+
+    /// <summary>
+    /// Update rotation of turret to move the turret left and right;
+    /// </summary>
+    private void UpdateRotation () {
+        transform.LookAt (player.transform);
+        Quaternion newRotation = transform.rotation;
+        newRotation.eulerAngles = new Vector3 (-90.0f, newRotation.eulerAngles.y, newRotation.eulerAngles.z);
+        transform.rotation = newRotation;
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerEnter (Collider other) {
+        if (other.gameObject.tag == "Player") {
+            lineRenderer.enabled = true;
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerExit (Collider other) {
+        if (other.gameObject.tag == "Player") {
+            lineRenderer.enabled = false;
+        }
+    }
+}
