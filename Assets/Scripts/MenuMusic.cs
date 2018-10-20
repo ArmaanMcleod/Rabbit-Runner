b6b843f114cf2823fa120ad14d2c1bdab411bb77
OpticGenius <opticgenius@hotmail.com>
2018-10-20 11:38:20 +1100

    Added background music for menu

---
+++ Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour {
+    private AudioSource audioSource;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake () {
+        audioSource = gameObject.GetComponent<AudioSource> ();
+        audioSource.loop = true;
+    }
+}
