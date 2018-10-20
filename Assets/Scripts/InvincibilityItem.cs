735e50f9d54984103ed0f132e17abf46797734f3
OpticGenius <opticgenius@hotmail.com>
2018-10-20 13:48:00 +1100

    Added audio effect on item pickup

--- Assets/Scripts/InvincibilityItem.cs
+++ Assets/Scripts/InvincibilityItem.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class InvincibilityItem : MonoBehaviour {
 
-    public void OnTriggerEnter(Collider other) {
-        if (other.tag.Equals("Player")) {
+    // The sound effect attached
+    private AudioSource audioSource;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake () {
+        audioSource = gameObject.GetComponent<AudioSource> ();
+    }
+
+    public void OnTriggerEnter (Collider other) {
+        if (other.tag.Equals ("Player")) {
+            AudioSource.PlayClipAtPoint (audioSource.clip, transform.position);
             GameObject player = other.attachedRigidbody.gameObject;
-            player.GetComponent<PlayerHealth>().ChangeInvincibility(true);
-            gameObject.SetActive(false);
+            player.GetComponent<PlayerHealth> ().ChangeInvincibility (true);
+            gameObject.SetActive (false);
         }
     }
 }
